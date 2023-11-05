199226d096f75debd71f3109601dcdb1fb5a55fb
Jinwoo Park <www.jwpark.co.kr@gmail.com>
2023-11-05 23:44:00 +0900

    Show image

--- upload.js
+++ upload.js
@@ -18,14 +18,23 @@ FilePond.registerPlugin(
 );
 
 // Select the file input and use create() to turn it into a pond
-const pond = FilePond.create(document.querySelector('input'));
+const uploader = FilePond.create(document.querySelector('input'), {
+    name: "uploader",
+    labelIdle: `
+        <div style="width:100%;height:100%;">
+            <p>Drag &amp; Drop your image or <span class="filepond--label-action" tabindex="0">Browse</span></p>
+        </div>
+    `
+});
 
 // Register an event for addfile
-pond.on('addfile', (error, file) => {
+uploader.on('addfile', (error, file) => {
     if (error) {
         console.log('Error during the file upload')
     }
+
     // Activate the popup and show the result
+    document.querySelector('.output-img').setAttribute('src', file.getFileEncodeDataURL())
     document.body.classList.add("active-popup");
 });
 
